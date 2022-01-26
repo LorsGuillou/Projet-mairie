abf416653c6f2f670b4bad75d08c4b4e69cb06fd
Laurent Guillou <laurent29190@hotmail.fr>
2022-01-26 10:50:05 +0100

    404 + drawio

---
+++ erreur/404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>404</title>
+</head>
+<body>
+    <div  id="erreur" class="container">
+        <h2>Erreur 404 !</h2>
+        <p>Hélas, nous n'avons pas pu accéder à votre requête...</p>
+    </div>
+</body>
+</html>
